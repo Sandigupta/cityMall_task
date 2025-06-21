a9964ef84f9ffd8507da9a3d917a7662a20b1cba
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-06-22 00:48:22 +0530

    Updated env

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // backend running here
+  baseURL: process.env.VITE_API_BASE_URL || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 // utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Backend socket server
-
+// const socket = io("http://localhost:5000"); // Backend socket server
+const socket = io(process.env.VITE_SOCKET_URL || "http://localhost:5000");
 export default socket;
